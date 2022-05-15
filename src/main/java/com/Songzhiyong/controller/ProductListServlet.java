98ddb3dde857fa7997d957c6e0fd64ff5df33651
Sonzhiyong <3231267175@qq.com>
2022-05-16 00:17:43 +0800

    12th week12 commit

--- src/main/java/com/Songzhiyong/controller/ProductListServlet.java
+++ src/main/java/com/Songzhiyong/controller/ProductListServlet.java
@@ -1,20 +1,36 @@
 package com.Songzhiyong.controller;
 
 
+import com.Songzhiyong.dao.ProductDao;
+import com.Songzhiyong.model.Product;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-@WebServlet(name = "ProductServletServlet", value = "/admin/productList")
+
+@WebServlet("/admin/productList")
 public class ProductListServlet extends HttpServlet {
+    Connection con = null;
     @Override
+    public void init()  {
+        con = (Connection) getServletContext().getAttribute("con");
+    }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String path="/WEB-INF/views/admin/productList.jsp";
+        try {
+            ProductDao productDao = new ProductDao();
+            List<Product> productList = productDao.findAll(con);
+            request.setAttribute("productList",productList);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        String path = "/WEB-INF/views/admin/productList.jsp";
         request.getRequestDispatcher(path).forward(request,response);
     }
 
-    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.doGet(request, response);
     }
